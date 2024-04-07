28d6e733fd556399d5988b4e5ba0ae00aaded1eb
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2024-04-07 09:51:17 +0500

    fs cb issue

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,5 +1,4 @@
 import { v2 as cloudinary } from "cloudinary";
-import fs from "fs";
 
 cloudinary.config({
   cloud_name: "dj1mhaagb",
@@ -10,7 +9,7 @@ cloudinary.config({
 
 const uploadOnCloundinary = async (localFilePath) => {
   try {
-    if (!localFilePath) return null;
+    // if (!localFilePath) return null;
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
