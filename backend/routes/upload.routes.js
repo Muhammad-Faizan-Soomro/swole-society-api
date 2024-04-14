eaf936d6e73aa3d6648cea05bf7e69e9dcd08ceb
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2024-04-14 19:23:34 +0500

    Update upload.routes.js

--- backend/routes/upload.routes.js
+++ backend/routes/upload.routes.js
@@ -44,21 +44,6 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage,
   limits: { fileSize: 1 * 1024 * 1024 }, // 1MB
-  fileFilter: (req, file, cb) => {
-    if (
-      file.mimetype == "image/png" ||
-      file.mimetype == "image/jpg" ||
-      file.mimetype == "image/jpeg" ||
-      file.mimetype == "image/webp"
-    ) {
-      cb(null, true);
-    } else {
-      cb(null, false);
-      const err = new Error("Only .png, .jpg and .jpeg format allowed!");
-      err.name = "ExtensionError";
-      return cb(err);
-    }
-  },
 });
 
 router.post("/", upload.array("image"), async (req, res) => {
